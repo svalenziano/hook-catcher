73d77a3e6901235415cc3868aa3d56521450c26c
SValenziano <senorvalenz@gmail.com>
2026-05-04 13:46:04 -0400

    wip: Add OTel collector

---
+++ server/src/otel/instrumentation.ts
@@ -0,0 +1,19 @@
+// https://opentelemetry.io/docs/languages/js/getting-started/nodejs/
+
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics';
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
